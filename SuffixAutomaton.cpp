879d0b7a42293453b0c241b96b018b135a9f763b
tmkkk <46168631+tmkkk@users.noreply.github.com>
2019-02-02 13:58:12 +0100

    Update SuffixAutomaton.cpp

--- SuffixAutomaton.cpp
+++ SuffixAutomaton.cpp
@@ -3,75 +3,72 @@
 #define siz(c) (int)(c).size()
 
 struct SuffixAutomaton {
-	struct Node
-	{
-		int len,link,cnt=1;
-		map<char,int>nxt;
-		Node(int len=0,int link=0,int cnt=1): 
-		len(len),link(link),cnt(cnt){}
-		int& operator[](char c){
+	struct Node {
+		int len, link, cnt=1;
+		map<char, int> nxt;
+		Node(int len=0, int link=0, int cnt=1): 
+		len(len), link(link), cnt(cnt) {}
+		int& operator[](char c) {
 			return nxt[c];
 		}
 	};
-	vector<Node>s;
+	vector<Node> s;
 	int lst;
-	void addChar(const char c){
-		s.emplace_back(s[lst].len+1);
-		int cur=siz(s)-1,p=lst;
-		while(p>-1 and !s[p][c]){
-			s[p][c]=cur;
+	void addChar(const char c) {
+		s.emplace_back(s[lst].len + 1);
+		int cur = siz(s) - 1, p = lst;
+		while(p > -1 and !s[p][c]){
+			s[p][c] = cur;
 			p=s[p].link;
 		}
-		if(p>-1){
-			int q=s[p][c];
-			if(s[q].len==s[p].len+1)
-				s[cur].link=q;
+		if(p > -1) {
+			int q = s[p][c];
+			if(s[q].len == s[p].len+1)
+				s[cur].link = q;
 			else {
-				int cl=siz(s);
+				int cl = siz(s);
 				s.emplace_back(s[q]);
-				s[cl].len=s[p].len+1;
-				s[q].link=s[cur].link=cl;
-				while(p>-1 and s[p][c]==q) {
-					s[p][c]=cl;
-					p=s[p].link;
+				s[cl].len = s[p].len+1;
+				s[q].link = s[cur].link = cl;
+				while(p > -1 and s[p][c] == q) {
+					s[p][c] = cl;
+					p = s[p].link;
 				}
 			}
 		}
-		lst=cur;
+		lst = cur;
 	}
-	void init(){
-		s.emplace_back(0,-1,0);
-		lst=0;
+	
+	SuffixAutomaton(): lst(0) {
+		s.emplace_back(0, -1, 0);
 	}
-	SuffixAutomaton(){
-		init();
-	}
-	SuffixAutomaton(const string& S){
-		init();
+	SuffixAutomaton(const string& S): lst(0) {
+		s.emplace_back(0, -1, 0);
 		for(auto& c:S)
 			addChar(c);
 	}
-	void dfs(int a,vector<bool>& vis){
-		vis[a]=true;
+	
+	void dfs(int a, vector<bool>& vis) {
+		vis[a] = true;
 		for(auto& it:s[a].nxt){
-			if(!vis[it.nd]) dfs(it.nd,vis);
-			s[a].cnt+=s[it.nd].cnt;
+			if(!vis[it.nd]) dfs(it.nd, vis);
+			s[a].cnt += s[it.nd].cnt;
 		}
 	}
 	void calcDP(){
-		vector<bool>vis(siz(s));
-		dfs(0,vis);
+		vector<bool> vis(siz(s));
+		dfs(0, vis);
 	}
 	string getKth(long long k){
-		int cur=0;
-		string ans="";
+		int cur = 0;
+		string ans = "";
 		while(k){
 			for(auto& it:s[cur].nxt){
-				if(s[it.nd].cnt<k)
-					k-=s[it.nd].cnt;
+				if(s[it.nd].cnt < k)
+					k -= s[it.nd].cnt;
 				else {
 					ans.push_back(it.st);
-					cur=it.nd;
+					cur = it.nd;
 					k--;
 					break;
 				}
@@ -83,7 +80,7 @@ struct SuffixAutomaton {
 
 int main()
 {
-	string S; cin>>S;
+	string S; cin >> S;
 	SuffixAutomaton sa(S);
 	sa.calcDP();
 	int q; cin>>q;
